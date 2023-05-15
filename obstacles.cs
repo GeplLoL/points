615d10f2fb37937d6b152d92fb4c494c32987d93
opilane <opilane@HP-CZC2349HT0>
2023-05-15 15:11:16 +0300

    obstacles

--- obstacles.cs
+++ obstacles.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace Snake
 {
-    class obstacles:Figure
+    internal class obstacles
     {
+        int mapWidht;
+        int mapHeight;
+        char sym;
+
+        Random random = new Random();
+
+        public obstacles(int mapWidht, int mapHeight, char sym)
+        {
+            this.mapWidht = mapWidht;
+            this.mapHeight = mapHeight;
+            this.sym = sym;
+        }
+
+        public Point CreateObstacles()
+        {
+            int x = random.Next(2, mapWidht - 2);
+            int y = random.Next(2, mapHeight - 2);
+            return new Point(x, y, sym);
+        }
     }
 }
